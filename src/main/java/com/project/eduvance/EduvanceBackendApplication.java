e4fb13b23b0529a17d7e38a97a545f7a5d8897fd
Sandeep <sandeepkumar090304@gmail.com>
2024-06-28 20:49:33 +0530

    commit 1

---
+++ src/main/java/com/project/eduvance/EduvanceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.project.eduvance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduvanceBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduvanceBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/eduvance/EduvanceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.eduvance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduvanceBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
