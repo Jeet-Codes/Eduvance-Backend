d618e1d2886cd993fe33dac7437752b4919e6bb6
Sandeep <sandeepkumar090304@gmail.com>
2024-07-05 00:02:50 +0530

    Email Done

--- src/main/java/com/project/eduvance/Service/ServiceImp/EmailService.java
+++ src/main/java/com/project/eduvance/Service/ServiceImp/EmailService.java
@@ -1,19 +1,22 @@
 package com.project.eduvance.Service.ServiceImp;
 
 import com.project.eduvance.Dto.EmailDetails;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 @Service
 @Slf4j
 public class EmailService {
 
-
+    @Autowired
     private JavaMailSender javaMailSender;
 
     @Value("${spring.mail.username}")
@@ -21,18 +24,25 @@ public class EmailService {
 
     public void sendSimpleMail(EmailDetails emailDetails) {
         try {
-            SimpleMailMessage message = new SimpleMailMessage();
-            message.setFrom(emailSender);
-            message.setTo(emailDetails.getRecipient());
-            message.setText(emailDetails.getMessageBody());
-            message.setSubject(emailDetails.getSubject());
+//            SimpleMailMessage message = new SimpleMailMessage();
+//            message.setFrom(emailSender);
+//            message.setTo(emailDetails.getRecipient());
+//            message.setText(emailDetails.getMessageBody());
+//            message.setSubject(emailDetails.getSubject());
 
-            javaMailSender.send(message);
+            MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+            MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage);
+            mimeMessageHelper.setTo(emailDetails.getRecipient());
+            mimeMessageHelper.setSubject(emailDetails.getSubject());
+            mimeMessageHelper.setText(emailDetails.getMessageBody());
+
+
+            javaMailSender.send(mimeMessage);
             log.info("Email sent successfully to " + emailDetails.getRecipient());
             log.info("Email sender " + emailSender);
 
 
-        }catch (MailException e) {
+        }catch (MailException | MessagingException e) {
             throw new RuntimeException(e);
         }
     }
