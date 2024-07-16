47a35f5b35defbe3a1b9418dc3175a16d394ae75
Sandeep <sandeepkumar090304@gmail.com>
2024-07-16 23:18:32 +0530

    Login panel User Service

--- src/main/java/com/project/eduvance/Entity/User.java
+++ src/main/java/com/project/eduvance/Entity/User.java
@@ -16,6 +16,7 @@ import lombok.Setter;
 public class User {
         @Id
         private String userId;
+        private String userName;
         private String userEmail;
         private String userPasswd;
 }
