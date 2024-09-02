40ec9770aa9a124f1f6c4127ba9f3f124e067211
Sandeep <sandeepkumar090304@gmail.com>
2024-09-02 23:47:17 +0530

    Management Panel 1.9.2 Rechecked CourseController,Degree Controller

--- src/main/java/com/project/eduvance/Controllers/CourseController.java
+++ src/main/java/com/project/eduvance/Controllers/CourseController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("eduvance/course")
+@RequestMapping("/eduvance/course")
 @CrossOrigin("*")
 public class CourseController {
 
