eb9d9eb5694298514dd16dd6ec3b3c44c9492fdb
Sandeep <sandeepkumar090304@gmail.com>
2024-09-11 14:03:29 +0530

    Swagger UI 1.0 Added the dependencies and Confeged

---
+++ src/main/java/com/project/eduvance/Config/OpenApiConfig.java
@@ -0,0 +1,29 @@
+package com.project.eduvance.Config;
+
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(
+        info = @Info(
+                contact = @Contact(
+                        name = "Sandeep Kumar",
+                        email = "sandeep090304@gmail.com",
+                        url = "https://github.com/Jeet-Codes/Eduvance-Backend.git"
+                ),
+                description = "OpenApi Documentation for Student",
+                title = "OpenApi specification - Sandeep",
+                termsOfService = "Term of Service"
+        ),
+        servers = @Server(
+                description = "Local ENV",
+                url = "http://localhost:8181"
+
+        )
+)
+public class OpenApiConfig {
+
+
+}
